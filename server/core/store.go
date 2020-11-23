e1920e527c67bc45730e1a584dde36f68050830e
disksing <i@disksing.com>
2020-11-23 14:50:33 +0800

    update store

--- server/core/store.go
+++ server/core/store.go
@@ -46,16 +46,20 @@ type StoreInfo struct {
 	lastPersistTime     time.Time
 	leaderWeight        float64
 	regionWeight        float64
+	hotReadWeight       float64
+	hotWriteWeight      float64
 	available           map[storelimit.Type]func() bool
 }
 
 // NewStoreInfo creates StoreInfo with meta data.
 func NewStoreInfo(store *metapb.Store, opts ...StoreCreateOption) *StoreInfo {
 	storeInfo := &StoreInfo{
-		meta:         store,
-		stats:        &pdpb.StoreStats{},
-		leaderWeight: 1.0,
-		regionWeight: 1.0,
+		meta:           store,
+		stats:          &pdpb.StoreStats{},
+		leaderWeight:   1.0,
+		regionWeight:   1.0,
+		hotReadWeight:  1.0,
+		hotWriteWeight: 1.0,
 	}
 	for _, opt := range opts {
 		opt(storeInfo)
@@ -78,6 +82,8 @@ func (s *StoreInfo) Clone(opts ...StoreCreateOption) *StoreInfo {
 		lastPersistTime:     s.lastPersistTime,
 		leaderWeight:        s.leaderWeight,
 		regionWeight:        s.regionWeight,
+		hotReadWeight:       s.hotReadWeight,
+		hotWriteWeight:      s.hotWriteWeight,
 		available:           s.available,
 	}
 
@@ -101,6 +107,8 @@ func (s *StoreInfo) ShallowClone(opts ...StoreCreateOption) *StoreInfo {
 		lastPersistTime:     s.lastPersistTime,
 		leaderWeight:        s.leaderWeight,
 		regionWeight:        s.regionWeight,
+		hotReadWeight:       s.hotReadWeight,
+		hotWriteWeight:      s.hotWriteWeight,
 		available:           s.available,
 	}
 
@@ -267,6 +275,16 @@ func (s *StoreInfo) GetLeaderWeight() float64 {
 // GetRegionWeight returns the Region weight of the store.
 func (s *StoreInfo) GetRegionWeight() float64 {
 	return s.regionWeight
+}
+
+// GetHotReadWeight returns the hot read weight of the store.
+func (s *StoreInfo) GetHotReadWeight() float64 {
+	return s.hotReadWeight
+}
+
+// GetHotWriteWeight returns the hot write weight of the store.
+func (s *StoreInfo) GetHotWriteWeight() float64 {
+	return s.hotWriteWeight
 }
 
 // GetLastHeartbeatTS returns the last heartbeat timestamp of the store.
